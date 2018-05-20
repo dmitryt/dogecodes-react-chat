cbc455f8ed1f4c83622c336a92337645e07b994b
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2018-05-20 23:34:06 +0300

    added config

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
+import { Form, Field } from 'react-final-form'
 
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
@@ -11,30 +12,54 @@ const styles = theme => ({
   },
 });
 
-const LoginForm = ({ classes, width, selectedChat }) => (
-  <form className={classes.container} noValidate autoComplete="off">
-    <TextField
-      label="Username"
-      placeholder="Type your username"
-      className={classes.textField}
-      margin="normal"
-      fullWidth
-      required
-    />
-    <TextField
-      label="Password"
-      placeholder="Type your password"
-      className={classes.textField}
-      margin="normal"
-      type="password"
-      fullWidth
-      required
-    />
-    <Button variant="raised" color="primary" fullWidth className={classes.button}>
-      Login
-    </Button>
-  </form>
-);
+class LoginForm extends React.Component {
+  onSubmit = values => {
+    console.log(JSON.stringify(values, 0, 2));
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Form
+        onSubmit={this.onSubmit}
+        render={({ handleSubmit, reset, submitting, pristine, values }) => (
+          <form className={classes.container} noValidate autoComplete="off">
+            <Field
+              label="Username"
+              placeholder="Type your username"
+              name="username"
+              type="text"
+              className={classes.textField}
+              margin="normal"
+              component={TextField}
+              fullWidth
+              required
+            />
+            <Field
+              label="Password"
+              placeholder="Type your password"
+              name="password"
+              type="password"
+              className={classes.textField}
+              margin="normal"
+              component={TextField}
+              fullWidth
+              required
+            />
+            <Button
+              className={classes.button}
+              variant="raised"
+              color="primary"
+              type="submit"
+              fullWidth
+            >
+              Login
+            </Button>
+          </form>
+        )}>
+      </Form>
+    );
+  }
+}
 
 LoginForm.propTypes = {
   classes: PropTypes.object.isRequired,
