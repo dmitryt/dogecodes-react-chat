d46cb67a9daa7cde7f7f24dbb6104c545d3d8331
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2018-05-22 21:21:27 +0300

    Added redux-logger, HMR, devtools

--- src/index.js
+++ src/index.js
@@ -6,5 +6,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+ReactDOM.render(<App />, rootEl);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    ReactDOM.render(<App />, rootEl);
+  })
+}
+
 registerServiceWorker();
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 
-import reducer from '../reducers';
+import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware(rootSaga);
-  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const enhancer = composeEnhancers(
+    applyMiddleware(sagaMiddleware, logger)
+  );
+  const store = createStore(rootReducer, enhancer);
+
+  if (process.env.NODE_ENV !== "production") {
+    if (module.hot) {
+      module.hot.accept('../reducers', () => {
+        store.replaceReducer(rootReducer);
+      });
+    }
+  }
 
   sagaMiddleware.run(rootSaga);
-  
+
   return store;
 }
