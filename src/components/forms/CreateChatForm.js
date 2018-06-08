ae14f0a114961494744e5134d57270e7a912b7d1
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2018-06-08 18:24:22 +0300

    added prettier + husky + lint-staged

--- src/components/forms/CreateChatForm.js
+++ src/components/forms/CreateChatForm.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 
 import Button from 'material-ui/Button';
 import Dialog, { DialogContent, DialogTitle } from 'material-ui/Dialog';
 import { Form, Field } from 'react-final-form';
 import TextField from './TextField';
-
-import { withStyles } from 'material-ui/styles';
 
 const styles = theme => ({
   title: {
@@ -36,58 +35,39 @@ const validate = ({ title }) => {
   return errors;
 };
 
-class CreateChatForm extends React.Component {
-  state = {
-    open: false,
-  };
-
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { classes, onSubmit, open, onClose } = this.props;
-    return (
-      <div>
-        <Dialog
-          open={open}
-          onClose={onClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <Form
-            onSubmit={onSubmit}
-            validate={validate}
-            render={({ handleSubmit, reset, submitting, pristine, values }) => (
-              <form className={classes.container} onSubmit={handleSubmit} noValidate autoComplete="off">
-                <DialogTitle className={classes.title}>Create new chat</DialogTitle>
-                <DialogContent className={classes.content}>
-                  <Field
-                    label="New Chat"
-                    placeholder="Type the title"
-                    name="title"
-                    type="text"
-                    margin="normal"
-                    component={TextField}
-                    autoComplete="title"
-                    fullWidth
-                    required
-                  />
-                  <Button color="primary" type="submit" disabled={submitting}>
-                    Create
-                  </Button>
-                </DialogContent>
-              </form>
-            )}>
-          </Form>
-        </Dialog>
-      </div>
-    );
-  }
-}
+const CreateChatForm = ({
+  classes, onSubmit, open, onClose,
+}) => (
+  <div>
+    <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+      <Form
+        onSubmit={onSubmit}
+        validate={validate}
+        render={({ handleSubmit, submitting }) => (
+          <form className={classes.container} onSubmit={handleSubmit} noValidate autoComplete="off">
+            <DialogTitle className={classes.title}>Create new chat</DialogTitle>
+            <DialogContent className={classes.content}>
+              <Field
+                label="New Chat"
+                placeholder="Type the title"
+                name="title"
+                type="text"
+                margin="normal"
+                component={TextField}
+                autoComplete="title"
+                fullWidth
+                required
+              />
+              <Button color="primary" type="submit" disabled={submitting}>
+                Create
+              </Button>
+            </DialogContent>
+          </form>
+        )}
+      />
+    </Dialog>
+  </div>
+);
 
 CreateChatForm.propTypes = {
   classes: PropTypes.object.isRequired,
--- src/mock-data.js
+++ src/mock-data.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import faker from 'faker';
 
 const shuffle = arr => arr.sort(() => Math.random() - 0.5);
@@ -7,30 +8,31 @@ const COUNT_OF_MESSAGES = 20;
 const COUNT_OF_STATUSES = 20;
 const COUNT_OF_OWN_MESSAGES = 20;
 
-const getTime = () => `${faker.random.number(10)} ${faker.random.arrayElement(['minutes', 'hours', 'seconds'])} ago`;
+const getTime = () =>
+  `${faker.random.number(10)} ${faker.random.arrayElement(['minutes', 'hours', 'seconds'])} ago`;
 const getColor = () => faker.internet.color(100, 100, 100);
 const getName = () => `${faker.name.firstName()} ${faker.name.lastName()}`;
 
 const ownName = getName();
 const ownColor = getColor();
 
-export const chatsData = Array.from({ length: COUNT_OF_CHATS }).map((_, i) => ({
+export const chatsData = Array.from({ length: COUNT_OF_CHATS }).map(() => ({
   id: faker.random.uuid(),
   title: faker.lorem.words(3),
   color: getColor(),
   time: getTime(),
 }));
 
-const userMessages = Array.from({ length: COUNT_OF_MESSAGES }).map((_, i) => ({
+const userMessages = Array.from({ length: COUNT_OF_MESSAGES }).map(() => ({
   id: faker.random.uuid(),
   color: getColor(),
   time: getTime(),
   username: getName(),
   message: faker.lorem.sentence(),
-  type: 'message'
+  type: 'message',
 }));
 
-const userOwnMessages = Array.from({ length: COUNT_OF_OWN_MESSAGES }).map((_, i) => ({
+const userOwnMessages = Array.from({ length: COUNT_OF_OWN_MESSAGES }).map(() => ({
   id: faker.random.uuid(),
   color: ownColor,
   time: getTime(),
@@ -40,13 +42,13 @@ const userOwnMessages = Array.from({ length: COUNT_OF_OWN_MESSAGES }).map((_, i)
   isOwn: true,
 }));
 
-const userStatuses = Array.from({ length: COUNT_OF_STATUSES }).map((_, i) => ({
+const userStatuses = Array.from({ length: COUNT_OF_STATUSES }).map(() => ({
   id: faker.random.uuid(),
   color: getColor(),
   time: getTime(),
   username: getName(),
   action: faker.random.arrayElement(['joined', 'left']),
-  type: 'info'
+  type: 'info',
 }));
 
 export const selectedChat = {
